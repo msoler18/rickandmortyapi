ad0ffb0e82874ed9ec15f2043e8178c635ba572a
Miguel Soler <msoler735@gmail.com>
2020-02-07 18:08:31 -0500

    Add api solution with callbacks

--- src/callbacks/api.js
+++ src/callbacks/api.js
@@ -1,16 +1,33 @@
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
+let API = 'https://rickandmortyapi.com/api/character/'
+
 
 function fecthData(url_api,callback){
   let xhttp = new XMLHttpRequest()
   xhttp.open('GET',url_api,true)
   xhttp.onreadystatechange = function(event){
-    if(xhttp.readyState === 4){
-      if(xhttp.status === 200){
+    if(this.readyState === 4){
+      if(this.status === 200){
         callback(null,JSON.parse(xhttp.responseText))
       } else {
         const error = new Error('Error in' + url_api)
-        return callback
+        return callback(error,null)
       }
     }
   }
+  xhttp.send()
 }
+
+
+fecthData(API,function(error,data){
+  if(error) return console.error(error)
+  fecthData(API + data.results[0].id, function (error1,data1){
+    if(error) return console.error(error1)
+    fecthData(data1.origin.url,function(error2,data2){
+      if(error) return console.error(error2)
+      console.log("Number of characters is:" + data.info.count) 
+      console.log("The name of character is:" + data1.name) 
+      console.log("Character dimension is:" + data2.dimension)
+    })  
+  })
+})
