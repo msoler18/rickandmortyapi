013cb23756e761bea699e5cd2c418d782ae4f209
Miguel Soler <msoler735@gmail.com>
2020-02-07 18:51:34 -0500

    Add promise all

--- src/promises/promises.js
+++ src/promises/promises.js
@@ -1,13 +1,40 @@
 const somethingWillHappen = () => {
   return new Promise((resolve,reject) => {
-    if(){
+    if(true){
       resolve("Ok promise is resolve")
-    } else{
+    } else {
       reject("Promise is reject")
     } 
   })
 }
 
-somethingWillHappen()
-  .then(resolve => console.log(resolve))
-  .catch(error => console.log(error))
+// somethingWillHappen()
+//   .then(resolve => console.log(resolve))
+//   .catch(error => console.log(error))
+
+
+const somethingWillHappen2 = () => {
+  return new Promise((resolve,reject) => {
+    if(true){
+      setTimeout(()=>{
+        
+      },3000)
+    } else {
+      const error = new Error('Promise is not fine')
+      reject(error)
+    }
+  })
+}
+
+
+// somethingWillHappen2()
+//   .then(response => console.log(response))
+//   .catch(error => console.log(error))
+
+Promise.all([somethingWillHappen(),somethingWillHappen2()])
+  .then(response => {
+    console.log("Array of response promises" + response)
+  })
+  .catch(error => {
+    console.log(error)
+  })
